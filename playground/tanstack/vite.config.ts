f9c84c54cec75139fe2e62c49e03b9cd01eb4f38
benten <ben@benten.ca>
2023-12-31 22:14:14 -0800

    chore: use node adaptor for examples

--- playground/tanstack/vite.config.ts
+++ playground/tanstack/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import vono from "../../src/index";
-import { netlify } from "../../src/adaptors";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vono({ adaptor: netlify({ edge: true }) })],
+  plugins: [react(), vono()],
   build: {
     rollupOptions: {
       input: "ui/entry.client.tsx",
