a7cb2346da91b38d4f8bf75b72cb01c3c40d9a31
Joseph Lozano <me@lozanojoseph.com>
2024-01-05 15:31:04 -0500

    add cloudflare adapter to cloudflare example

--- examples/cloudflare/vite.config.ts
+++ examples/cloudflare/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import vono from "../../src/index"
+import cloudflare from "../../src/adapters/cloudflare"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vono()],
+  plugins: [react(), vono({adapter: cloudflare()})],
 })
