7212de920d15918269d71e15bcb2731d40d9f3e0
Joseph Lozano <me@lozanojoseph.com>
2024-01-05 15:46:38 -0500

    link cloudflare adapter

--- examples/cloudflare/vite.config.ts
+++ examples/cloudflare/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import vono from "../../src/index"
-import cloudflare from "../../src/adapters/cloudflare"
+import vono from "@gaiiaa/vono"
+import { cloudflare } from "@gaiiaa/vono/adapters"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vono({adapter: cloudflare()})],
+  plugins: [react(), vono({ adapter: cloudflare() })],
 })
