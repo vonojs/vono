99233d107fa2977097396df3e9170127acc01968
benten <ben@benten.ca>
2024-01-19 02:04:33 -0800

    chore: remove registerServer

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,4 @@
 import vono from "./vite";
 
-declare global {
-  var __vono: {
-    servers: {
-      path: string;
-    }[];
-  };
-}
-
-function registerServer(args: { path: string }) {
-  globalThis.__vono ??= {
-    servers: [],
-  };
-  globalThis.__vono.servers.push(args);
-}
-
-export { registerServer };
 export { useVFS } from "./vfs";
 export default vono;
