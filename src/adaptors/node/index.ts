fee7373ea673a92da5d0df93e5deb640c5cd9154
benten <ben@benten.ca>
2024-04-07 20:27:06 -0600

    fix: resolving to ts in node adaptor

--- src/adaptors/node/index.ts
+++ src/adaptors/node/index.ts
@@ -7,6 +7,6 @@ const dir = resolveModuleDirectory(import.meta.url);
 export default class Node extends Adaptor {
 	name = "node";
 
-	developmentRuntime = join(dir, "runtime.dev.ts");
-	productionRuntime = join(dir, "runtime.prod.ts");
+	developmentRuntime = join(dir, "runtime.dev");
+	productionRuntime = join(dir, "runtime.prod");
 }
