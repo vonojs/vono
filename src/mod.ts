37567637e6a8f1bc41b3c08ff1aab284c01bad99
benten <ben@benten.ca>
2024-05-15 00:15:36 -0700

    fix: wrong import path

--- src/mod.ts
+++ src/mod.ts
@@ -329,7 +329,7 @@ function assets(config: {
 			vfs.add({
 				path: "/assets",
 				serverContent: async () => {
-					const getAsset = `export async function getModuleInfo (path){ path.startsWith("/") && (path = path.slice(1)); if (import.meta.env.DEV) { const res = await fetch(\`http://localhost:5173/__fetch_asset?mod=\${path}\`); if (!res.ok) {throw new Error("Failed to fetch assets")}; return await res.json();}; const manifest = (await import("#vono/manifest")).default; return manifest[path]};`
+					const getAsset = `export async function getModuleInfo (path){ path.startsWith("/") && (path = path.slice(1)); if (import.meta.env.DEV) { const res = await fetch(\`http://localhost:5173/__fetch_asset?mod=\${path}\`); if (!res.ok) {throw new Error("Failed to fetch assets")}; return await res.json();}; const manifest = (await import("#vono/assets")).manifest; return manifest[path]};`
 					if (isBuild) {
 						const path = typeof config.manifest === "function" ? config.manifest() : config.manifest;
 						return `export const manifest = ${JSON.stringify(await getBuildManifest(p.join(vite.build.outDir, path)),)};${getAsset}`;
