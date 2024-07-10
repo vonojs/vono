129e433aa4d64ef57e0f48aa1daeb843ff4d1081
Benton <ben@benten.ca>
2024-07-10 01:11:42 -0600

    fix: exports

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "@benstack/builder";
 import dtsPlugin from "esbuild-plugin-d.ts";
 
 export default defineConfig({
-	// entryPoints: ["src/mod.ts", "src/adaptors/**/*.ts", "src/runtime.ts"],
 	entryPoints: ["src/**/*.*"],
-	external: ["#vono", "vite"],
+	external: ["#vono", "./src/*"],
 	bundle: true,
 	outdir: "dist",
 	platform: "node",
