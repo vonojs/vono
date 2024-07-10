31026b6195294d33cebca10c0bb10cd5dc96c3ad
Benton <ben@benten.ca>
2024-07-10 01:46:49 -0600

    fix: exports

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from "@benstack/builder";
+import { Plugin } from "esbuild";
 import dtsPlugin from "esbuild-plugin-d.ts";
+
+const bundleDepsOnly: () => Plugin = () => ({
+	name: "bundle-deps-only",
+	setup(build) {
+		build.onResolve({ filter: /^\./ }, (args) => {
+			return {
+				external: true,
+				path: args.path,
+			}
+		})
+	}
+})
 
 export default defineConfig({
 	entryPoints: ["src/**/*.*"],
-	external: ["#vono", "./src/*"],
+	external: ["#vono"],
 	bundle: true,
 	outdir: "dist",
 	platform: "node",
 	format: "esm",
 	target: "esnext",
-	plugins: [dtsPlugin()],
+	plugins: [dtsPlugin(), bundleDepsOnly],
 });
